4c1d9c1c3d5d2d3b7acdd038f363b48804cbe1ca
rodiv1 <zeomo@ukr.net>
2016-01-21 14:14:22 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+
+/**
+ * Created by Vlad on 14.01.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int num = 14;
+        boolean isPrime;
+
+        if(num < 2) isPrime = false;
+        else isPrime = true;
+
+        for(int i = 2; i <= num/i; i ++){
+            if((num % i) == 0){
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime) System.out.println("Простое число");
+        else System.out.println("Не простое число");
+    }
+}
