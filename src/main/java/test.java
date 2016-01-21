4d06b649dc0fed4471f2111fad42b012ce2e72cd
Vlad <otis_13@ukr.net>
2016-01-21 23:24:24 +0200

    tes

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -4,9 +4,9 @@
 public class test {
     public static void main(String[] args) {
     int sum = 0;
-    int nums[][] = new int[3][5];
+    int nums[][] = new int[4][5];
 
-        for(int i = 0; i < 3; i ++)
+        for(int i = 0; i < 4; i ++)
             for(int j = 0; j < 5; j ++)
                 nums[i][j] = (i+1) * (j+1);
 
