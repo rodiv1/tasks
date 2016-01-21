cfadf7b2fdb6bfee5cd2ac97ee8ada24b4c2dd0c
Vlad <otis_13@ukr.net>
2016-01-21 19:06:18 +0200

    tes

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,6 +3,19 @@
  */
 public class test {
     public static void main(String[] args) {
+    int sum = 0;
+    int nums[][] = new int[3][5];
 
+        for(int i = 0; i < 3; i ++)
+            for(int j = 0; j < 5; j ++)
+                nums[i][j] = (i+1) * (j+1);
+
+        for(int x[] : nums){
+            for(int y : x){
+                System.out.println("Значення рівне: " + y);
+                sum += y;
+            }
+        }
+        System.out.println("Сума: " + sum);
     }
 }
