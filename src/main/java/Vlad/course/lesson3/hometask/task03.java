a1c9042428142e09dae4ca837fccacd1205d9da8
Гудвін <zeomo@ukr.net>
2016-08-16 15:55:47 +0300

    Гра хрестики-нолики

--- src/main/java/Vlad/course/lesson3/hometask/task03.java
+++ src/main/java/Vlad/course/lesson3/hometask/task03.java
@@ -1,98 +1,121 @@
 package Vlad.course.lesson3.hometask;
 
-import java.util.Arrays;
-import java.util.Random;
+
 import java.util.Scanner;
 
 /**
- * Created by Гудвін on 02.08.2016.
+ * Created by Гудвін on 02.08.2016.+
+ * Написать игру «Крестики нолики» используя двухмерные массивы.
  */
 public class task03 {
     public static void main(String[] args) {
-        byte dig, err = 0;
-        char [] s = new char[10];
-        int rand, ag = 1;
-        Scanner sel = new Scanner(System.in);
-        Scanner again = new Scanner(System.in);
-        Random r = new Random();
+        char[][] arr = new char[3][3];
 
-        System.out.println("Number of cell:");
-        System.out.println("7 | 8 | 9");
-        System.out.println("4 | 5 | 6");
-        System.out.println("1 | 2 | 3");
-        System.out.println(" ");
-        System.out.println(" ");
-        System.out.println(" ");
+        do {
 
-        while((ag == 1) || (ag == 1)) {
-
-            do {
-                System.out.println("Enter number of cell: ");
-                err = 0;
-                dig = sel.nextByte();
-                if ((dig < 0) || (dig > 10)) System.out.println("Enter valid value!!!");
-                if (s[dig] != '\u0000') {err = 1; System.out.println("Enter free cell!!!");}
-
-            }while ((dig < 0) || (dig > 10) || (err != 0));
-
-            s[dig] = 'X';
-
-            do{
-                rand = r.nextInt(9) + 1;
-                if ((s[rand] != '\u0000') || (rand == 10)) err = 2; //ошибка, если ячейка занята
-                else {s[rand] = 'O'; err = 0;}
-            }while (err != 0);
-
-            System.out.println(s[7] + " | " + s[8] + " | " + s[9]);
-            System.out.println(s[4] + " | " + s[5] + " | " + s[6]);
-            System.out.println(s[1] + " | " + s[2] + " | " + s[3]);
-            System.out.println(" ");
-            System.out.println(" ");
-
-            if (((s[1] == 'X') && (s[2] == 'X') && (s[3] == 'X')) || ((s[4] == 'X') && (s[5] == 'X') && (s[6] == 'X')) || ((s[7] == 'X') && (s[8] == 'X') && (s[9] == 'X')) || ((s[1] == 'X') && (s[5] == 'X') && (s[9] == 'X')) || ((s[3] == 'X') && (s[5] == 'X') && (s[7] == 'X'))) // X
-            {
-                System.out.println("You WIN!!!");
-                System.out.println("Again? (1 = yes/0 = no)");
-                ag = again.nextByte();
-                if (ag == 0 || ag == 0)
-                    break;
-                else
-                {
-                    ag = 1;
-                    Arrays.fill(s, '\u0000');
-
+            for (int i = 1; i < 10; i++) {
+                System.out.print("[" + i + "]");
+                if (i == 3 || i == 6) {
+                    System.out.println();
+                }
+            }
+            System.out.println();
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    arr[i][j] = '_';
                 }
             }
 
-            if (((s[1] == 'O') && (s[2] == 'O') && (s[3] == 'O')) || ((s[4] == 'O') && (s[5] == 'O') && (s[6] == 'O')) || ((s[7] == 'O') && (s[8] == 'O') && (s[9] == 'O')) || ((s[1] == 'O') && (s[5] == 'O') && (s[9] == 'O')) || ((s[3] == 'O') && (s[5] == 'O') && (s[7] == 'O'))) // O
-            {
-                System.out.println("You LOSE!!!");
-                System.out.println("Again? (1 = yes/0 = no)");
-                ag = again.nextByte();
-                if (ag == 0 || ag == 0) {
-                    break;
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    System.out.print(arr[i][j] + "\t");
                 }
-                else
-                {
-                    ag = 1;
-                    Arrays.fill(s, '\u0000');
+                System.out.println();
+            }
+        }while (false);
+        Scanner sc = new Scanner(System.in);
+        int count = 0, variation = 0;
+        boolean bool = true;
+        do {
+            if (count == 0 || count % 2 == 0){
+                System.out.println("x:");
+                variation = sc.nextInt();
+                switch (variation){
+                    case 1: arr[0][0] = 'x';
+                        break;
+                    case 2: arr[0][1] = 'x';
+                        break;
+                    case 3: arr[0][2] = 'x';
+                        break;
+                    case 4: arr[1][0] = 'x';
+                        break;
+                    case 5: arr[1][1] = 'x';
+                        break;
+                    case 6: arr[1][2] = 'x';
+                        break;
+                    case 7: arr[2][0] = 'x';
+                        break;
+                    case 8: arr[2][1] = 'x';
+                        break;
+                    case 9: arr[2][2] = 'x';
+                        break;
+                }
+                for (int i = 0; i < arr.length; i++) {
+                    for (int j = 0; j < arr[i].length; j++) {
+                        System.out.print(arr[i][j] + "\t");
+                    }
+                    System.out.println();
+                }
+                }else if (count % 2 != 0){
+                System.out.println("0:");
+                variation = sc.nextInt();
+                switch (variation){
+                    case 1: arr[0][0] = '0';
+                        break;
+                    case 2: arr[0][1] = '0';
+                        break;
+                    case 3: arr[0][2] = '0';
+                        break;
+                    case 4: arr[1][0] = '0';
+                        break;
+                    case 5: arr[1][1] = '0';
+                        break;
+                    case 6: arr[1][2] = '0';
+                        break;
+                    case 7: arr[2][0] = '0';
+                        break;
+                    case 8: arr[2][1] = '0';
+                        break;
+                    case 9: arr[2][2] = '0';
+                        break;
+                }
+                for (int i = 0; i < arr.length; i++) {
+                    for (int j = 0; j < arr[i].length; j++) {
+                        System.out.print(arr[i][j] + "\t");
+                    }
+                    System.out.println();
+                }
                 }
 
+            if (arr[0][0] == 'x' && arr[0][1] == 'x' && arr[0][2] == 'x' || arr[1][0] == 'x' && arr[1][1] == 'x' && arr[1][2] == 'x' ||
+                    arr[2][0] == 'x' && arr[2][1] == 'x' && arr[2][2] == 'x' || arr[0][0] == 'x' && arr[1][0] == 'x' && arr[2][0] == 'x'
+                    || arr[0][1] == 'x' && arr[1][1] == 'x' && arr[2][1] == 'x' || arr[0][2] == 'x' && arr[1][2] == 'x' && arr[2][2] == 'x'
+                    || arr[0][0] == 'x' && arr[1][1] == 'x' && arr[2][2] == 'x' || arr[0][2] == 'x' && arr[1][1] == 'x' && arr[2][0] == 'x'){
+                System.out.println("X win");
+                bool = false;
+            }else if (arr[0][0] == '0' && arr[0][1] == '0' && arr[0][2] == '0' || arr[1][0] == '0' && arr[1][1] == '0' && arr[1][2] == '0' ||
+                    arr[2][0] == '0' && arr[2][1] == '0' && arr[2][2] == '0' || arr[0][0] == '0' && arr[1][1] == '0' && arr[1][2] == '0' ||
+                    arr[0][2] == '0' && arr[1][1] == '0' && arr[2][0] == '0' || arr[0][2] == '0' && arr[1][2] == '0' && arr[2][2] == '0' ||
+                    arr[0][0] == '0' && arr[1][0] == '0' && arr[2][0] == '0' ||arr[0][1] == '0' && arr[1][1] == '0' && arr[2][1] == '0'){
+                System.out.println("0 win");
+                bool = false;
             }
 
-            if ((s[1] != '\u0000') && (s[2] != '\u0000') && (s[3] != '\u0000') && (s[4] != '\u0000') && (s[5] != '\u0000') && (s[6] != '\u0000') && (s[7] != '\u0000') && (s[8] != '\u0000') && (s[9] != '\u0000')){
-                System.out.println("Dead heat!!!");
-                System.out.println("Again? (1 = yes/0 = no)");
-                ag = again.nextByte();
-                if (ag == 0 || ag == 0) {
-                    break;
-                }
-                else
-                {
-                    ag = 1;
-                    Arrays.fill(s, '\u0000');
-                }
+        count++;
+            if (count==8){
+                System.out.println("Nobody won!");
             }
-        }
+        }while (bool);
+
     }
 }
---
+++ src/main/java/Vlad/testprog/test01.java
@@ -0,0 +1,17 @@
+package Vlad.testprog;
+
+/**
+ * Created by Гудвін on 13.08.2016.
+ */
+public class test01 {
+    public static void main(String[] args) {
+        int[][] arr = new int[15][5];
+        for (int i = 0; i < arr.length; i++){
+            for (int j = 0; j <arr[i].length; j++){
+
+            }
+            System.out.println(arr[i].length);
+        }
+
+    }
+}
